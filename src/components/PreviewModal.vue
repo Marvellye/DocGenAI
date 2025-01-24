54d7e4fa27c48d7086d7ae23dec830118f5fa369
Marvellous Ezekiel <114520580+Marvellye@users.noreply.github.com>
2025-01-25 00:12:25 +0100

    Initial commit from your app

--- src/components/PreviewModal.vue
+++ src/components/PreviewModal.vue
@@ -1,9 +1,27 @@
+```
+```vue
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue';
+
+const props = defineProps<{
   fileUrl: string
 }>()
 
-defineEmits(['close'])
+const emit = defineEmits(['close'])
+
+const isPdf = computed(() => {
+  return props.fileUrl.toLowerCase().endsWith('.pdf');
+});
+
+const downloadFile = () => {
+  const link = document.createElement('a');
+  link.href = props.fileUrl;
+  link.download = props.fileUrl.substring(props.fileUrl.lastIndexOf('/') + 1);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  emit('close');
+};
 </script>
 
 <template>
@@ -18,12 +36,24 @@ defineEmits(['close'])
           ✕
         </button>
       </div>
-      <div class="flex-1 p-4">
+      <div class="flex-1 p-4" v-if="isPdf">
         <iframe
           :src="`https://docs.google.com/gview?url=${encodeURIComponent(fileUrl)}&embedded=true`"
           class="w-full h-full rounded-lg bg-white"
         ></iframe>
       </div>
+      <div v-else class="flex-1 p-4 flex items-center justify-center">
+        <button @click="downloadFile" class="px-6 py-3 bg-green-500 text-black font-medium rounded-lg hover:bg-green-400 transition-colors">
+          Download Document
+        </button>
+      </div>
+      <div class="p-4 flex justify-end" v-if="isPdf">
+        <button @click="downloadFile" class="px-6 py-3 bg-green-500 text-black font-medium rounded-lg hover:bg-green-400 transition-colors">
+          Download Document
+        </button>
+      </div>
     </div>
   </div>
 </template>
+```
+```html
